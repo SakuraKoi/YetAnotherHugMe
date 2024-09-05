13ed0430d08720cb282f11c8dfd135686166612e
SakuraKooi <me@sakurakooi.dev>
2024-09-05 13:43:01 +0800

    feat: add server plugin base

---
+++ paper_serverside/src/main/java/dev/sakurakooi/yetanotherhugme/YetAnotherHugMe.java
@@ -0,0 +1,17 @@
+package dev.sakurakooi.yetanotherhugme;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YetAnotherHugMe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
