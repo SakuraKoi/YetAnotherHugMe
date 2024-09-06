dabde6f53507a072318e97efaa5ad46e8d436605
SakuraKooi <me@sakurakooi.dev>
2024-09-06 10:50:14 +0800

    feat: add item

--- paper_serverside/src/main/java/dev/sakurakooi/yetanotherhugme/YetAnotherHugMe.java
+++ paper_serverside/src/main/java/dev/sakurakooi/yetanotherhugme/YetAnotherHugMe.java
@@ -2,14 +2,24 @@ package dev.sakurakooi.yetanotherhugme;
 
 import dev.sakurakooi.yetanotherhugme.listeners.HandshakeListener;
 import dev.sakurakooi.yetanotherhugme.network.HugMeHandshakeHandler;
+import dev.sakurakooi.yetanotherhugme.utils.ItemBuilder;
+import io.papermc.paper.event.server.ServerResourcesReloadedEvent;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
 import java.util.UUID;
 
-public final class YetAnotherHugMe extends JavaPlugin {
+public final class YetAnotherHugMe extends JavaPlugin implements Listener {
+    private final NamespacedKey RECIPE_KEY = new NamespacedKey(this, "hug_ticket");
+
     @Getter
     private static YetAnotherHugMe instance;
 
@@ -20,11 +30,40 @@ public final class YetAnotherHugMe extends JavaPlugin {
     public void onEnable() {
         instance = this;
         Bukkit.getPluginManager().registerEvents(new HandshakeListener(), this);
+        Bukkit.getPluginManager().registerEvents(this, this);
         Bukkit.getServer().getMessenger().registerIncomingPluginChannel(this, "yetanotherhugme:handshake", new HugMeHandshakeHandler());
+        registerRecipe();
+    }
+
+    private void registerRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(RECIPE_KEY, ItemBuilder.createTicket());
+        recipe.shape("BBB", "WWW", "PPP");
+        recipe.setIngredient('B', Material.LIGHT_BLUE_WOOL);
+        recipe.setIngredient('W', Material.WHITE_WOOL);
+        recipe.setIngredient('P', Material.PINK_WOOL);
+        recipe.setGroup("HugMe");
+        Bukkit.getServer().addRecipe(recipe);
+    }
+
+    private void unregisterRecipe() {
+        Bukkit.getServer().removeRecipe(RECIPE_KEY, true);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        unregisterRecipe();
+    }
+
+    @EventHandler
+    public void onLogin(PlayerJoinEvent e) {
+        if (!e.getPlayer().hasDiscoveredRecipe(RECIPE_KEY)) {
+            e.getPlayer().discoverRecipe(RECIPE_KEY);
+        }
+    }
+
+    @EventHandler
+    public void onServerResourcesReload(ServerResourcesReloadedEvent event) {
+        unregisterRecipe();
+        registerRecipe();
     }
 }
---
+++ paper_serverside/src/main/java/dev/sakurakooi/yetanotherhugme/utils/ItemBuilder.java
@@ -0,0 +1,33 @@
+package dev.sakurakooi.yetanotherhugme.utils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.Arrays;
+
+public class ItemBuilder {
+    private static NamespacedKey namespacedKey = new NamespacedKey("yetanotherhugme", "hug_ticket");
+    public static ItemStack createTicket() {
+        ItemStack itemStack = new ItemStack(Material.PAPER, 1);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setEnchantmentGlintOverride(true);
+        meta.setMaxStackSize(1);
+        meta.displayName(Component.text("Hug Ticket").color(TextColor.color(0xff4081)).decorate(TextDecoration.BOLD));
+        meta.lore(Arrays.asList(
+                Component.text("Right click to hug someone!").color(TextColor.color(0xc6ff00)),
+                Component.text("You can only use this ticket once.")));
+        meta.getPersistentDataContainer().set(namespacedKey, PersistentDataType.BOOLEAN, true);
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
+
+    public static boolean isHugTicket(ItemStack itemStack) {
+        return itemStack.getItemMeta().getPersistentDataContainer().has(namespacedKey, PersistentDataType.BOOLEAN);
+    }
+}
